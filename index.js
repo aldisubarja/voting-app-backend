788e168b6ae50fa03a66e56a8458679344520a39
aldisubarja <aldisubarja@gmail.com>
2025-04-03 16:17:50 +0700

    add cors handling

--- index.js
+++ index.js
@@ -2,9 +2,23 @@ const express = require('express');
 const { mongoURI, port } = require('./app/modules/config');
 const routeWrapper = require('./app/modules/routeWrapper');
 const { connectMongoDB } = require('./app/modules/mongo');
-
+const cors = require('cors');
+const allowedOrigins = ['http://localhost:5173'];
 const app = express();
+
 app.use(express.json());
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
+}));
+
 app.use('/', routeWrapper);
 app.listen(port, () => console.log(`Server running on port ${port}`));
 
